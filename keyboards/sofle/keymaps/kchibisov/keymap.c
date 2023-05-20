0655b3b920009fda9f9e561a2f8f58fa414dd20c
Kirill Chibisov <contact@kchibisov.com>
2023-05-20 07:13:56 +0300

    kchibisov: update keymap

--- keyboards/sofle/keymaps/kchibisov/keymap.c
+++ keyboards/sofle/keymaps/kchibisov/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------| Mute  |    | ADJST |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LGUI | LAlt | LCTR | MO(1) | /Space  /       \Enter \  |MO(2) | RCTR | RAlt | RGUI |
+ *            | LGUI | LAlt | LCTR | MO(1) |/Space  /       \Enter \  |MO(2) | RCTR | RAlt | RGUI |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
@@ -28,12 +28,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_INS,  \
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_LBRC, \
   KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT, \
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_MUTE,   TO(_ADJUST), KC_N, KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT, \
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,   TO(_ADJUST),  KC_MUTE, KC_N, KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT, \
                  KC_LGUI,KC_LALT,KC_LCTRL, MO(_LOWER), KC_SPC,       KC_ENT,  MO(_RAISE), KC_RCTRL,KC_RALT, KC_RGUI            \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |      |      |                    |  F6  |  F7  |  F8  |  F9  | F10  | F12  |
+ * |      |      |      |      |      |      |                    |  F7  |  F8  |  F9  |  F10  | F11 | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      |      |      |      |      |                    | TRNS | TRNS | COPY | TRNS | Right|   \  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [_LOWER] = LAYOUT( \
-  KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                         KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F12,  \
+  KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                         KC_F7,   KC_F8,   KC_F9,   KC_F10,    KC_F11,  KC_F12,  \
   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                         KC_TRNS, KC_TRNS, KC_COPY, KC_TRNS,  KC_RGHT, KC_BSLS, \
   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                         KC_TRNS, KC_LEFT, KC_PLUS, KC_RPRN,  KC_EQL,  KC_RBRC, \
   KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,       KC_TRNS, KC_BRIU, KC_BRID, KC_TRNS, KC_PASTE, KC_SYRQ, KC_TRNS, \
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |  F11 |  F1  |  F2  |  F3  |  F4  |  F5  |                    |      |      |      |      |      |      |
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      | MPRV | MPLY | MPRV | HOME |  END |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -67,31 +67,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [_RAISE] = LAYOUT( \
-  KC_F11,  KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,                         KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, \
+  KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,                         KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, \
   KC_NO,   KC_MPRV,  KC_MPLY, KC_MNXT, KC_HOME, KC_END,                        KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, \
   KC_NO,   KC_MINUS, KC_LPRN, KC_UNDS, KC_PGUP, KC_PGDN,                       KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, \
   KC_TRNS, KC_NO,    KC_NO,   KC_DOWN, KC_UP,   KC_CUT, KC_TRNS,      KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, \
                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_BSPC,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS              \
 ),
 /* ADJUST
- * ,-------------------------------------------------.                          ,-----------------------------------------.
+ * ,------------------------------------------------.                          ,--------------------------------------------.
  * |      |RGB_M_P|RGB_M_B|RGB_M_R|RGB_M_SW|RGB_M_SN|                          |RGB_M_K|RGB_M_X|RGB_M_G|      |      |      |
  * |------+-------+-------+-------+--------+--------|                          |-------+-------+-------+------+------+------|
  * |      |       |       |       |        |        |                          |       |       |       |      |      |      |
  * |------+-------+-------+-------+--------+--------|                          |------+--------+-------+------+------+------|
- * |      |       |       |       |        |        |-------.          ,-------|       |       |       |      |      |      |
+ * |      |RGB_HUI|RGB_SAI|RGB_VAI|RGB_TOG |        |-------.          ,-------|       |       |       |      |      |      |
  * |------+-------+-------+-------+--------+--------|  TRNS |          | BASE  |------+--------+-------+------+------+------|
  * | TRNS |       |       |       |        |        |-------|          |-------|       |       |       |      |      | TRNS |
- * `-----------------------------------------/       /                  \      \-----------------------------------------'
- *            | TRNS | TRNS | TRNS | TRNS | / TRNS  /                    \ TRNS \  | TRNS | TRNS | TRNS | TRNS |
- *            |      |      |      |      |/       /                      \      \ |      |      |      |      |
- *            `----------------------------------'                        '------''---------------------------'
+ * `------------------------------------------------/       /           \      \-----------------------------------------'
+ *                   | TRNS | TRNS | TRNS | TRNS | / TRNS  /             \ TRNS \  | TRNS | TRNS | TRNS | TRNS |
+ *                   |      |      |      |      |/       /               \      \ |      |      |      |      |
+ *                   `----------------------------------'                 '------''---------------------------'
  */
 [_ADJUST] = LAYOUT( \
   KC_NO,   RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN,                     RGB_M_K, RGB_M_X, RGB_M_G, KC_NO, KC_NO, KC_NO, \
   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, \
   KC_NO,   RGB_HUI, RGB_SAI, RGB_VAI, RGB_TOG,  KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, \
-  KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,  KC_TRNS,   TO(_BASE), KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, \
+  KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,  TO(_BASE),   KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, \
                     KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS              \
 ),
 };
@@ -303,18 +303,10 @@ bool oled_task_user(void) {
 #ifdef ENCODER_ENABLE
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) {
-        if (clockwise) {
-            tap_code(KC_VOLD);
-        } else {
-            tap_code(KC_VOLU);
-        }
-    } else if (index == 1) {
-        if (clockwise) {
-            tap_code(KC_PGDN);
-        } else {
-            tap_code(KC_PGUP);
-        }
+    if (clockwise) {
+        tap_code(KC_VOLD);
+    } else {
+        tap_code(KC_VOLU);
     }
     return true;
 }
