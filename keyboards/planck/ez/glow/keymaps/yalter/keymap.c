0fcfc91b755b0c0f0c2b56e88f41ac885a329bfa
Ivan Molodetskikh <yalterz@gmail.com>
2023-08-08 15:14:36 +0400

    Put PSCR on the base layer

--- keyboards/planck/ez/glow/keymaps/yalter/keymap.c
+++ keyboards/planck/ez/glow/keymaps/yalter/keymap.c
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    KC_ESC,  MSP_AGR, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  XXXXXXX, RAISE,   KC_LGUI, KC_LBRC, KC_RBRC, KC_ENT
+    KC_ESC,  MSP_AGR, KC_PSCR, KC_LALT, LOWER,   KC_SPC,  XXXXXXX, RAISE,   KC_LGUI, KC_LBRC, KC_RBRC, KC_ENT
   ),
 
   [_LOWER] = LAYOUT_planck_grid(
@@ -101,7 +101,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_ADJUST] = LAYOUT_planck_grid(
-    _______, _______, _______, _______, KC_PSCR, TFH,     _______, _______, _______, _______, KC_PAUS, _______,
+    _______, _______, _______, _______, _______, TFH,     _______, _______, _______, _______, KC_PAUS, _______,
     _______, _______, AU_ON,   AU_OFF,  AU_TOG,  _______, LC_TOGG, RGB_TOG, RGB_VAI, RGB_VAD, _______, QK_BOOT,
     _______, _______, _______, _______, VSRG,    _______, NK_TOGG, RGB_MOD, RGB_HUI, RGB_HUD, _______, _______,
     _______, _______, _______, _______, _______, _______, XXXXXXX, _______, _______, _______, _______, _______
