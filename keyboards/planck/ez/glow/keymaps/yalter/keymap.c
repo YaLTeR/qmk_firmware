0d8960406e880d7adb655fb0cfef33b5f9a1e60a
Ivan Molodetskikh <yalterz@gmail.com>
2023-10-08 14:36:02 +0400

    Add a Colemak Mod-DH layer

--- keyboards/planck/ez/glow/keymaps/yalter/keymap.c
+++ keyboards/planck/ez/glow/keymaps/yalter/keymap.c
@@ -66,6 +66,7 @@ enum tap_dance_codes {
 
 enum planck_layers {
   _BASE,
+  _COLDH,
   _LOWER,
   _RAISE,
   _ADJUST,
@@ -77,12 +78,20 @@ enum planck_layers {
 #define RAISE MO(_RAISE)
 #define TFH TG(_TFH)
 #define VSRG TG(_VSRG)
+#define COLDH TG(_COLDH)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    KC_ESC,  MSP_AGR, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  XXXXXXX, RAISE,   KC_LGUI, KC_LBRC, KC_RBRC, KC_ENT
+  ),
+
+  [_COLDH] = LAYOUT_planck_grid(
+    KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
+    KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
     KC_ESC,  MSP_AGR, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  XXXXXXX, RAISE,   KC_LGUI, KC_LBRC, KC_RBRC, KC_ENT
   ),
 
@@ -103,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_ADJUST] = LAYOUT_planck_grid(
     _______, _______, _______, _______, _______, TFH,     _______, _______, _______, _______, KC_PAUS, QK_RBT,
     _______, _______, AU_ON,   AU_OFF,  AU_TOG,  _______, LC_TOGG, RGB_TOG, RGB_VAI, RGB_VAD, _______, QK_BOOT,
-    _______, _______, _______, _______, VSRG,    _______, NK_TOGG, RGB_MOD, RGB_HUI, RGB_HUD, _______, _______,
+    _______, _______, _______, COLDH,   VSRG,    _______, NK_TOGG, RGB_MOD, RGB_HUI, RGB_HUD, _______, _______,
     _______, _______, _______, _______, _______, _______, XXXXXXX, _______, _______, _______, _______, _______
   ),
 
